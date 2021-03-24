563d7229857a3a8f5fbbeba7967a401ae4118fa0
Martti Malmi <sirius@iki.fi>
2021-03-24 16:14:44 +0200

    works

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const IRIS_PRIVATE_KEY = JSON.parse(process.env.IRIS_PRIVATE_KEY || 'null');
-const PEERS = JSON.parse(process.env.PEERS || 'null') || 'https://iris.cx/gun';
+const PEERS = process.env.PEERS && process.env.PEERS.split(',');
 const INCOMING_HTTP_PORT = 3000;
 
 // process.env.GOOGLE_APPLICATION_CREDENTIALS should be the file path of a JSON containing the service account key
@@ -11,14 +11,18 @@ var firebaseApp = admin.initializeApp({
 });
 
 const Gun = require('gun');
-require('gun/sea');
+const SEA = require('gun/sea');
 
 const express = require('express');
 const app = express();
-const publicState = new Gun(PEERS);
-publicState.user().auth(IRIS_PRIVATE_KEY);
+const publicState = new Gun({peers:PEERS});
+const user = publicState.user();
+user.auth(IRIS_PRIVATE_KEY);
 
 app.get('/auth', async (req, res) => {
+  if (!(IRIS_PRIVATE_KEY && PEERS)) {
+    return res.status(400).send({message: 'bad server config'});
+  }
   const idToken = req.query.token;
   const pub = req.query.pub;
   if (idToken && pub) {
@@ -27,12 +31,13 @@ app.get('/auth', async (req, res) => {
     .verifyIdToken(idToken)
     .then((decodedToken) => {
       const uid = decodedToken.uid;
-      publicState.user().get('uidToPub').get(uid).put(pub);
-      publicState.user().get('follow').get(pub).put(true);
+      user.get('uidToPub').get(uid).put(pub);
+      user.get('follow').get(pub).put(true);
       console.log('linked user', pub.slice(0,6), '... to uid', uid.slice(0,6), '...');
       res.send('');
     })
     .catch((error) => {
+      res.status(400).send({message: 'infernal server error'});
       console.error('firebase authentication failure: ', error);
     });
   }
